e839032f387be43870dcaa5c7137c0ffc0edc979
Zer0 <48999715+Zer0-h@users.noreply.github.com>
2023-07-04 19:53:46 +0200

    Updated implementation

---
+++ Capitol 3 - IMPLEMENTACIÓ/Quatre en ratlla/pit.py
@@ -0,0 +1,47 @@
+import Arena
+from MCTS import MCTS
+from connect4.Connect4Game import Connect4Game
+from connect4.pytorch.NNet import NNetWrapper as NNet
+from connect4.pytorch.NNet import NNetWrapper as nn
+from connect4.Connect4Players import *
+
+
+import numpy as np
+from utils import *
+
+"""
+use this script to play any two agents against each other, or play manually with
+any agent.
+"""
+
+human_vs_cpu = False
+
+g = Connect4Game()
+
+# all players
+rp = RandomPlayer(g).play
+gp = OneStepLookaheadConnect4Player(g).play
+hp = HumanConnect4Player(g).play
+
+# nnet players
+n1 = NNet(g)
+
+args1 = dotdict({'numMCTSSims': 50, 'cpuct':1.0})
+mcts1 = MCTS(g, n1, args1)
+n1p = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
+
+if human_vs_cpu:
+    player2 = hp
+else:
+    n2 = NNet(g)
+    args2 = dotdict({'numMCTSSims': 50, 'cpuct': 1.0})
+    mcts2 = MCTS(g, n2, args2)
+    n2p = lambda x: np.argmax(mcts2.getActionProb(x, temp=0))
+
+    player2 = n2p  # Player 2 is neural network if it's cpu vs cpu.
+
+print(n1p)
+print(dotdict({'numMCTSSims': 50, 'cpuct': 1.0}))
+#arena = Arena.Arena(n1p, player2, g, display=Connect4Game.display)
+
+#print(arena.playGames(2, verbose=True))
